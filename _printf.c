8d58f9757050d2d8a6fa4a997e8476f6aedbc703
DYozar <79377769+DYozar@users.noreply.github.com>
2023-04-25 23:10:25 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,69 +1,66 @@
-#include <stdio.h>
-#include <stdarg.h> // for variadic arguments
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Custom implementation of printf function
- * @format: A format string
- * Return: The number of characters printed
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
-#include <stdio.h>
-#include <stdarg.h>
-
 int _printf(const char *format, ...)
 {
-int printed_chars = 0;
-va_list args;
-char ch;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-va_start(args, format);
+	if (format == NULL)
+		return (-1);
 
-for (int i = 0; format && format[i]; i++)
-{
-if (format[i] != '%')
-{
-putchar(format[i]);
-printed_chars++;
-continue;
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
 
-i++;
-
-ch = format[i];
-switch (ch)
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
 {
-case 'c':
-putchar(va_arg(args, int));
-printed_chars++;
-break;
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
 
-case 's':
-{
-char *str = va_arg(args, char *);
-if (!str)
-str = "(null)";
-while (*str)
-{
-putchar(*str);
-printed_chars++;
-str++;
-}
-}
-break;
-
-case '%':
-putchar('%');
-printed_chars++;
-break;
-
-default:
-putchar('%');
-putchar(ch);
-printed_chars += 2;
-break;
-}
-}
-
-va_end(args);
-
-return printed_chars;
+	*buff_ind = 0;
 }
