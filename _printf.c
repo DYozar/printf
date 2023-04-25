78b35443d0cd753aec4c98223aaa547ab174c1c6
DYozar <79377769+DYozar@users.noreply.github.com>
2023-04-25 23:09:41 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -11,59 +11,59 @@
 
 int _printf(const char *format, ...)
 {
-    int printed_chars = 0;
-    va_list args;
-    char ch;
+int printed_chars = 0;
+va_list args;
+char ch;
 
-    va_start(args, format);
+va_start(args, format);
 
-    for (int i = 0; format && format[i]; i++)
-    {
-        if (format[i] != '%')
-        {
-            putchar(format[i]);
-            printed_chars++;
-            continue;
-        }
+for (int i = 0; format && format[i]; i++)
+{
+if (format[i] != '%')
+{
+putchar(format[i]);
+printed_chars++;
+continue;
+}
 
-        i++;
+i++;
 
-        ch = format[i];
-        switch (ch)
-        {
-            case 'c':
-                putchar(va_arg(args, int));
-                printed_chars++;
-                break;
+ch = format[i];
+switch (ch)
+{
+case 'c':
+putchar(va_arg(args, int));
+printed_chars++;
+break;
 
-            case 's':
-                {
-                    char *str = va_arg(args, char *);
-                    if (!str)
-                        str = "(null)";
-                    while (*str)
-                    {
-                        putchar(*str);
-                        printed_chars++;
-                        str++;
-                    }
-                }
-                break;
+case 's':
+{
+char *str = va_arg(args, char *);
+if (!str)
+str = "(null)";
+while (*str)
+{
+putchar(*str);
+printed_chars++;
+str++;
+}
+}
+break;
 
-            case '%':
-                putchar('%');
-                printed_chars++;
-                break;
+case '%':
+putchar('%');
+printed_chars++;
+break;
 
-            default:
-                putchar('%');
-                putchar(ch);
-                printed_chars += 2;
-                break;
-        }
-    }
+default:
+putchar('%');
+putchar(ch);
+printed_chars += 2;
+break;
+}
+}
 
-    va_end(args);
+va_end(args);
 
-    return printed_chars;
+return printed_chars;
 }
