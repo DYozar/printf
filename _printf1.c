712ec689ea482de1346f8927dc7309e2dba0c265
DYozar <79377769+DYozar@users.noreply.github.com>
2023-06-26 15:27:49 +0200

    Update _printf1.c

--- _printf1.c
+++ _printf1.c
@@ -9,56 +9,56 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
+va_list args;
+int count = 0;
 
-    va_start(args, format);
+va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == '\0')
-                break;
-            else if (*format == 'c')
-            {
-                char c = va_arg(args, int);
-                write(1, &c, 1);
-                count++;
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(args, char *);
-                int len = 0;
-                while (str[len])
-                {
-                    write(1, &str[len], 1);
-                    len++;
-                    count++;
-                }
-            }
-            else if (*format == '%')
-            {
-                write(1, "%", 1);
-                count++;
-            }
-            else
-            {
-                write(1, "%", 1);
-                write(1, format, 1);
-                count += 2;
-            }
-        }
-        else
-        {
-            write(1, format, 1);
-            count++;
-        }
-        format++;
-    }
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+if (*format == '\0')
+break;
+else if (*format == 'c')
+{
+char c = va_arg(args, int);
+write(1, &c, 1);
+count++;
+}
+else if (*format == 's')
+{
+char *str = va_arg(args, char *);
+int len = 0;
+while (str[len])
+{
+write(1, &str[len], 1);
+len++;
+count++;
+}
+}
+else if (*format == '%')
+{
+write(1, "%", 1);
+count++;
+}
+else
+{
+write(1, "%", 1);
+write(1, format, 1);
+count += 2;
+}
+}
+else
+{
+write(1, format, 1);
+count++;
+}
+format++;
+}
 
-    va_end(args);
+va_end(args);
 
-    return count;
+return count;
 }
